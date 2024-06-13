13b29f3f0c3bdeaa74c5fce9b984ef2705b4513a
wken5577 <wken5577@hanmail.net>
2024-06-13 15:11:51 +0900

    eureca server, eureca client, openfeign을 활용한 ms 서비스간 통신

--- section8/accounts/src/main/java/com/eazybytes/accounts/mapper/CustomerMapper.java
+++ section8/accounts/src/main/java/com/eazybytes/accounts/mapper/CustomerMapper.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.mapper;
 
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.entity.Customer;
 
@@ -19,4 +20,10 @@ public class CustomerMapper {
         return customer;
     }
 
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto) {
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDto;
+    }
 }
