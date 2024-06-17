30d2e23c226cc768b0bc09a2ca000e9a40977803
wken5577 <wken5577@hanmail.net>
2024-06-17 20:07:27 +0900

    custom routing gateway server

--- section9-api-geteway/gatewayserver/src/main/java/com/msa/gatewayserver/GatewayserverApplication.java
+++ section9-api-geteway/gatewayserver/src/main/java/com/msa/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.msa.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,21 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator easyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+			.route(p -> p
+				.path("/eazybank/accounts/**")
+				.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}") )
+				.uri("lb://ACCOUNTS"))
+			.route(p -> p
+				.path("/eazybank/loans/**")
+				.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}") )
+				.uri("lb://LOANS"))
+			.route(p -> p
+				.path("/eazybank/cards/**")
+				.filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}") )
+				.uri("lb://CARDS"))
+			.build();
+	}
 }
