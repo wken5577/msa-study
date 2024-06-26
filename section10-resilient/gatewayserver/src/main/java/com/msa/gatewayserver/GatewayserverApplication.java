fb9711154290ef97a841cf8a378dfd3b158ae64b
wken5577 <wken5577@hanmail.net>
2024-06-26 14:22:38 +0900

    resilience4j circuitbreaker pattern

---
+++ section10-resilient/gatewayserver/src/main/java/com/msa/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,40 @@
+package com.msa.gatewayserver;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator easyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+			.route(p -> p
+				.path("/eazybank/accounts/**")
+				.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
+					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+					.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+						.setFallbackUri("forward:/contactSupport")))
+				.uri("lb://ACCOUNTS"))
+			.route(p -> p
+				.path("/eazybank/loans/**")
+				.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
+					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://LOANS"))
+			.route(p -> p
+				.path("/eazybank/cards/**")
+				.filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
+					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://CARDS"))
+			.build();
+	}
+}
---
+++ section10-resilient/gatewayserver/src/main/java/com/msa/gatewayserver/controller/FallbackController.java
@@ -0,0 +1,15 @@
+package com.msa.gatewayserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+	@RequestMapping("/contactSupport")
+	public Mono<String> contactSupport() {
+		return Mono.just("An error occurred while processing your request. Please contact support.");
+	}
+}
